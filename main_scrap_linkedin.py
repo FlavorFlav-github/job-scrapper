698c71b9262999558fb7da15c92667c9fd83b605
Yassine EL HAOUARI <yassine.elhaouari@outlook.fr>
2024-11-15 20:27:48 +0100

    Void

--- main_scrap_linkedin.py
+++ main_scrap_linkedin.py
@@ -88,21 +88,20 @@ def get_headers(url, template):
 
 
 def check_captcha():
-    def wait_for_response(timeout=60):
+    def wait_for_response():
         response_file = "../linkedin-scrap-jobs-data/captcha_check.json"
-        start_time = time.time()
         response = None
-        while time.time() - start_time < timeout:
-            if os.path.isfile(response_file):
-                try:
-                    with open(response_file, "r") as f:
-                        captcha_check_json = json.load(f)
-                        if "img_name" in captcha_check_json:
-                            response = captcha_check_json["img_name"]
-                            break
-                except Exception as e:
-                    print(f"Could not open captcha check json file {e}")
-            time.sleep(1)
+        time.sleep(30)
+
+        if os.path.isfile(response_file):
+            try:
+                with open(response_file, "r") as f:
+                    captcha_check_json = json.load(f)
+                    if "img_name" in captcha_check_json:
+                        response = captcha_check_json["img_name"]
+            except Exception as e:
+                print(f"Could not open captcha check json file {e}")
+
         with open(response_file, "w") as f:
             json.dump({}, f)
         return response  # Timeout if no response
