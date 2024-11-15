a7411576a0ff5f64754620f1f0265a05c4cc5021
Yassine EL HAOUARI <yassine.elhaouari@outlook.fr>
2024-11-15 12:42:17 +0100

    Void

--- telegramHook.py
+++ telegramHook.py
@@ -5,14 +5,17 @@ from telegram.ext import CommandHandler, Application, ContextTypes, MessageHandl
 
 
 def wait_captcha_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if "image" in update.message.text:
-        with open("../linkedin-scrap-jobs-data/captcha_check.json", "w") as f:
-            json.dump({"img_name": update.message.text}, f)
+    if len(context.args) > 0:
+        raw_param = context.args[0]
+        if "image" in raw_param:
+            with open("../linkedin-scrap-jobs-data/captcha_check.json", "w") as f:
+                json.dump({"img_name": update.message.text}, f)
 
 
 def main() -> None:
     application = Application.builder().token(const.telegrambottoken).build()
 
+    application.add_handler(CommandHandler("captcha_check", wait_captcha_response, has_args=True))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
